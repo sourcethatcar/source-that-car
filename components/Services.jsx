7079cd453166c9055c2d5177ecd4863f3c70722b
Suneet Misra <hello@suneet.codes>
2021-02-21 20:38:24 +0000

    Added basic layout for services section

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,15 +1,98 @@
 import styled from "@emotion/styled"
+import PropTypes from "prop-types"
 import { Layout } from "./Layout"
+import { FaCarSide, FaMoneyBillAlt, FaHandshake } from "react-icons/fa"
 
 const ServicesWrapper = styled.section`
   background-color: var(--colorWhite);
+  .description-container {
+    padding: 1rem 0;
+    display: grid;
+    column-gap: 6rem;
+    row-gap: 5rem;
+    grid-template-columns: repeat(auto-fit, minmax(260px, 1fr));
+  }
+
+  .services-container {
+    padding: 3rem 0;
+  }
+
+  /* .services-container {
+    h1 {
+      padding: 2rem 0;
+    }
+  } */
+
+  .description-title {
+    padding: 1rem 0;
+  }
+
+  .icon-container {
+    padding-top: 2rem;
+  }
+
+  .description-text {
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 1.75rem;
+    letter-spacing: 0em;
+    text-align: left;
+  }
 `
+
+const Description = ({ children, title, icon }) => {
+  return (
+    <div className="description-item">
+      <div className="icon-container">{icon}</div>
+      <h3 className="description-title">{title}</h3>
+      <p className="description-text">{children}</p>
+    </div>
+  )
+}
+
+Description.propTypes = {
+  children: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+}
 
 export const Services = () => {
   return (
     <ServicesWrapper id="services">
-      <Layout>
-        <h1>Our Services</h1>
+      <Layout className="services-layout">
+        <div className="services-container">
+          <h1>Our Service</h1>
+          <div className="description-container">
+            <Description
+              title="Your Choice, Your Car"
+              icon={<FaCarSide fill="#fca311" size="2rem" />}
+            >
+              We’re a family-run company based in Kent, UK with strong
+              relationships with local dealerships, built up over many years.
+              This means that we’re able to source your next new or used
+              vehicle, often with large discounts as compared to dealership
+              prices.
+            </Description>
+            <Description
+              title="Financing"
+              icon={<FaMoneyBillAlt fill="#fca311" size="2rem" />}
+            >
+              We have a variety of finacning options available for you to choose
+              from, at competitive rates. Part exchange of your current vehicle
+              can also be handled by us making your next purchase experience a
+              breeze!
+            </Description>
+            <Description
+              title="Bespoke Service"
+              icon={<FaHandshake fill="#fca311" size="2rem" />}
+            >
+              Your requirements are at the core of the service that we provide.
+              We work directly with you to determine the next car of yor dreams,
+              and then we leverage our partner network to find it at the best
+              price possible. We do the hard work, so that you don’t have to.
+            </Description>
+          </div>
+        </div>
       </Layout>
     </ServicesWrapper>
   )
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -11,6 +11,11 @@ const MyApp = ({ Component, pageProps }) => {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/modern-normalize/1.0.0/modern-normalize.min.css"
         />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Global styles={globalStyles} />
       <Component {...pageProps} />
