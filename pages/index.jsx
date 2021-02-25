b078c8b14b5bdc5d3861c94279a787750d6a9702
Suneet Misra <hello@suneet.codes>
2021-02-25 06:16:42 +0000

    Footer, navigation

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,23 @@
-import { Hero, Services, Testimonials, Contact, Footer } from "../components"
+import { useState } from "react"
+import {
+  Hero,
+  Services,
+  Testimonials,
+  Contact,
+  Footer,
+  Navigation,
+  NavigationMobile,
+} from "../components"
 
 const Home = () => {
+  const [clicked, setClicked] = useState(false)
+  const toggleMenu = () => setClicked(!clicked)
+  const closeMenu = () => setClicked(false)
+
   return (
     <div style={{ position: "relative" }}>
+      <Navigation toggleMenu={toggleMenu} />
+      <NavigationMobile clicked={clicked} closeMenu={closeMenu} />
       <Hero />
       <Services as="section" id="services">
         <h1>Our Service</h1>
