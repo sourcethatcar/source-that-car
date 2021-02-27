396ebcb83ca04ddf2a62692c86ef9276527869e2
Suneet Misra <hello@suneet.codes>
2021-02-27 12:16:36 +0000

    Added Contact section component

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,14 +1,47 @@
 import styled from "@emotion/styled"
 import { Layout } from "./Layout"
+import { Input } from "./Input"
 
 const ContactWrapper = styled.section`
   background-color: var(--colorWhite);
+  .form-container {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 3rem 0;
+  }
+  .name-fields {
+    display: flex;
+    width: 100%;
+    flex-wrap: wrap;
+  }
 `
 export const Contact = () => {
   return (
     <ContactWrapper id="contact">
       <Layout>
         <h1>Contact</h1>
+        <div className="form-container">
+          <form>
+            <div className="name-fields">
+              <Input
+                type="text"
+                fieldName="first-name"
+                placeholder="first name"
+              />
+              <Input
+                type="text"
+                fieldName="last-name"
+                placeholder="last name"
+              />
+            </div>
+            <Input type="email" fieldName="email" placeholder="email address" />
+            <Input
+              textArea
+              fieldName="comments"
+              placeholder="tell us how we can help..."
+            />
+          </form>
+        </div>
       </Layout>
     </ContactWrapper>
   )
