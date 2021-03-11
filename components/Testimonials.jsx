17c35728370364357d570cf0cf87551640d8ef4a
Suneet Misra <suneet@verse.co.uk>
2021-03-11 09:24:42 +0000

    moved testimonials test data into utils file and fixed horizontal scroll issue emanating from hero and testimonials section

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -1,30 +1,10 @@
 import styled from "@emotion/styled"
 import { Layout, Slider } from "../components"
-
-const testimonials = [
-  {
-    profileImg: "https://i.pravatar.cc/150?img=5",
-    text:
-      "Everyone at Source That Car have been fantastic. They were able to get the car of my dreams at a really fair price and I wouldn't hesitate recommending them to anyone in need of a car.",
-    author: "Bill Murray-Baggins",
-  },
-  {
-    profileImg: "https://i.pravatar.cc/150?img=8",
-    text:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id inventore deserunt odit officiis porro voluptate eligendi tempora commodi quos consequuntur iste sit quia, voluptatum amet dolores aperiam laborum ut illo!",
-    author: "John Goodman",
-  },
-  {
-    profileImg: "https://i.pravatar.cc/150?img=9",
-    text:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id inventore deserunt odit officiis porro voluptate eligendi tempora commodi quos consequuntur iste sit quia, voluptatum amet dolores aperiam laborum ut illo!",
-    author: "Prince Rogers-Nelson",
-  },
-]
+import testimonials from "../utils/testimonials"
 
 const TestimonialsWrapper = styled.section`
   background-color: var(--colorGrey);
-  width: 100vw;
+  width: 100%;
   .testimonials-layout {
     h1 {
       padding-bottom: 2rem;
---
+++ utils/testimonials.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    profileImg: "https://i.pravatar.cc/150?img=5",
+    text:
+      "Everyone at Source That Car have been fantastic. They were able to get the car of my dreams at a really fair price and I wouldn't hesitate recommending them to anyone in need of a car.",
+    author: "Bill Murray-Baggins",
+  },
+  {
+    profileImg: "https://i.pravatar.cc/150?img=8",
+    text:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id inventore deserunt odit officiis porro voluptate eligendi tempora commodi quos consequuntur iste sit quia, voluptatum amet dolores aperiam laborum ut illo!",
+    author: "John Goodman",
+  },
+  {
+    profileImg: "https://i.pravatar.cc/150?img=9",
+    text:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id inventore deserunt odit officiis porro voluptate eligendi tempora commodi quos consequuntur iste sit quia, voluptatum amet dolores aperiam laborum ut illo!",
+    author: "Prince Rogers-Nelson",
+  },
+]
