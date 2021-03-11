eadc637add1cc07e8c544b8a8c709dc877f90b33
Suneet Misra <hello@suneet.codes>
2021-03-11 06:06:22 +0000

    Added hack to temporarily disabled slide transition during window resizing, corrected svg dimension values within services section:

--- hooks/useWidth.jsx
+++ hooks/useWidth.jsx
@@ -1,3 +1,13 @@
+/** Aside from using setWidth() all logic within the eventHandler below is related to
+ * removing the transition property on the slider track during the resizing action.
+ * We set it to none first and then we add a timer to add back the transition value
+ * once resizing has ended. It's a bit hacky but currently there's no other way to
+ * detect when a resize event has finished
+ *
+ * https://css-tricks.com/snippets/jquery/done-resizing-event/
+ * https://stackoverflow.com/questions/5489946/how-to-wait-for-the-end-of-resize-event-and-only-then-perform-an-action
+ *
+ */
 import { useState, useEffect } from "react"
 
 export const useWidth = (elementRef) => {
@@ -9,8 +19,19 @@ export const useWidth = (elementRef) => {
     /** create a function to retrieve the width value from the DOM element being watched  */
     const getWidth = () => elementRef.current.getBoundingClientRect().width
 
-    /** create an eventHandler to be invoked upon window resizing */
-    const handleResize = () => setWidth(getWidth())
+    /** create an eventHandler to be invoked upon window resizing.
+     */
+    const handleResize = () => {
+      // eslint-disable-next-line prefer-const
+      let resizeTimer
+      elementRef.current.style.transition = "none"
+      setWidth(getWidth())
+
+      clearTimeout(resizeTimer)
+      resizeTimer = setTimeout(() => {
+        elementRef.current.style.transition = "all 0.25s ease-in-out"
+      }, 250)
+    }
 
     /** Set the initial value of state upon component mount */
     if (elementRef.current) {
