ff83370acd07553b556d528e1b7bdfb8e2d8f56f
Suneet Misra <hello@suneet.codes>
2021-04-04 10:39:04 +0100

    Changed layout on hero section and added social icons

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,5 +1,8 @@
 import styled from "@emotion/styled"
 import { Layout } from "./index"
+import { breakpoints } from "../styles"
+import { BsChevronDown } from "react-icons/bs"
+import { SiTwitter, SiFacebook, SiInstagram } from "react-icons/si"
 
 const HeroWrapper = styled.section`
   height: 100vh;
@@ -15,39 +18,134 @@ const HeroWrapper = styled.section`
     right: 0;
     z-index: -10;
     object-fit: cover;
+    filter: brightness(75%);
   }
 
   .headline-container {
-    height: 100%;
-    width: 100%;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
     align-items: center;
 
     h1 {
       color: var(--colorWhite);
-      background-color: var(--colorYellow);
-      padding: 1rem;
+      padding: 1rem 0;
       border-radius: 5px;
-      font-weight: bold;
+      font-weight: 500;
+      font-size: 3rem;
       text-align: center;
       font-style: italic;
-      text-transform: uppercase;
+      text-transform: capitalize;
       opacity: 0.9;
     }
+
+    h3 {
+      color: var(--colorWhite);
+    }
+  }
+  @media only screen and (min-width: ${breakpoints.tablet}) {
+    .headline-container h1 {
+      font-size: 4rem;
+    }
+  }
+
+  @media only screen and (min-width: ${breakpoints.desktop}) {
+    .headline-container {
+      align-items: flex-end;
+      h1 {
+        font-size: 5rem;
+      }
+    }
+  }
+
+  .social-icons-container {
+    display: flex;
+    padding: 1rem 0;
+
+    a {
+      padding: 0 1rem;
+
+      svg {
+        width: 2rem;
+        height: 2rem;
+        fill: var(--colorYellow);
+      }
+    }
+  }
+
+  .hero-arrow {
+    position: absolute;
+    bottom: 0;
+    padding: 1rem 0;
+    right: 50%;
+    width: 6rem;
+    height: 6rem;
+  }
+
+  @keyframes pulse_animation {
+    0% {
+      transform: scale(1) translateX(50%);
+    }
+    30% {
+      transform: scale(1) translateX(50%);
+    }
+    40% {
+      transform: scale(1.08) translateX(50%);
+    }
+    50% {
+      transform: scale(1) translateX(50%);
+    }
+    60% {
+      transform: scale(1) translateX(50%);
+    }
+    70% {
+      transform: scale(1.05) translateX(50%);
+    }
+    80% {
+      transform: scale(1) translateX(50%);
+    }
+    100% {
+      transform: scale(1) translateX(50%);
+    }
+  }
+
+  .pulse {
+    animation-name: pulse_animation;
+    animation-duration: 5000ms;
+    transform-origin: 70% 70%;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
   }
 `
 export const Hero = () => {
   return (
     <HeroWrapper as="section" id="hero">
-      <img
-        className="hero-background-image"
-        src="/images/mercedes.jpg"
-        alt="hero-car"
-      />
+      <picture>
+        <source
+          srcSet="/images/vw-mobile.jpg"
+          media={`(max-width: ${breakpoints.desktop})`}
+        />
+        <source srcSet="/images/vw-desktop.jpg" />
+        <img
+          className="hero-background-image"
+          src="/images/vw-desktop.jpg"
+          alt="hero-car"
+        />
+      </picture>
       <Layout className="headline-container">
-        <h1>Where will your next adventure take you?</h1>
+        <h1>Source That Car</h1>
+        <div className="social-icons-container">
+          <a href="https://www.twitter.com">
+            <SiTwitter />
+          </a>
+          <a href="https://www.google.com">
+            <SiInstagram />
+          </a>
+          <a href="https://www.google.com">
+            <SiFacebook />
+          </a>
+        </div>
       </Layout>
+      <BsChevronDown fill="white" className="pulse hero-arrow" />
     </HeroWrapper>
   )
 }
