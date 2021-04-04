ecfe6212f416ab346cc098feb94c4db60f0ee1e2
Suneet Misra <hello@suneet.codes>
2021-04-04 10:40:43 +0100

    Added string into testimonials section

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -9,13 +9,26 @@ const TestimonialsWrapper = styled.section`
     h1 {
       padding: 3rem 0;
     }
+    & > p {
+      font-size: 0.875rem;
+      font-style: italic;
+      text-align: center;
+      span {
+        font-weight: bold;
+      }
+    }
   }
 `
 export const Testimonials = () => {
   return (
     <TestimonialsWrapper id="testimonials">
-      <Layout className="testimonials-layout">
-        <h1>Testimonials</h1>
+      <Layout className="testimonials-layout" style={{ paddingBottom: "3rem" }}>
+        <h1>Feedback</h1>
+        <p>
+          We are <span>very</span> proud of the relationship we have with our
+          clients and are fortunate to have many repeat and referral business
+          opportunities.
+        </p>
         <Slider slides={testimonials} />
       </Layout>
     </TestimonialsWrapper>
