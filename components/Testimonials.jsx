6b07628ef308fa31b06e056c90f1406421674441
Suneet Misra <hello@suneet.codes>
2021-07-13 14:46:30 +0100

    Added trustpilot link and logo in testimonials

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled"
 import { Layout, Slider } from "../components"
+import TrustPilot from "./icons/TrustPilot"
 import PropTypes from "prop-types"
 
 const TestimonialsWrapper = styled.section`
@@ -16,6 +17,15 @@ const TestimonialsWrapper = styled.section`
       span {
         font-weight: bold;
       }
+      a {
+        color: var(--colorYellow);
+        svg {
+          display: inline-block;
+          height: 0.875rem;
+          width: 0.875rem;
+          fill: var(--colorYellow);
+        }
+      }
     }
   }
 `
@@ -24,12 +34,26 @@ export const Testimonials = ({ testimonials }) => {
     <TestimonialsWrapper id="testimonials">
       <Layout className="testimonials-layout" style={{ paddingBottom: "3rem" }}>
         <h1>Feedback</h1>
-        <p>
+        <p style={{ paddingBottom: "1rem" }}>
           We are <span>very</span> proud of the relationship we have with our
           clients and are fortunate to have many repeat and referral business
           opportunities.
         </p>
         <Slider slides={testimonials} />
+        <p style={{ paddingTop: "1rem" }}>
+          If you are a previous client, we would be grateful if you could leave
+          us a review on our{" "}
+          <span>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://uk.trustpilot.com/review/sourcethatcar.com"
+            >
+              <TrustPilot /> Trustpilot
+            </a>
+          </span>{" "}
+          page.
+        </p>
       </Layout>
     </TestimonialsWrapper>
   )
